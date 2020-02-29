d60cc46b37d13677306da2d467f3af4d61fdce5f
Austin Miller <austin.miller@colorado.edu>
2020-02-29 19:50:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,8 +64,8 @@ def reduce_to_all_true(source_array)
 def reduce_to_any_true(source_array)
     index = 0 
   while index < source_array.length do 
-    if source_array[index] 
-      return true 
+    if source_array[index] == false 
+      return false 
     end 
     index += 1 
   end 
