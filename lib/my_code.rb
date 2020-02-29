f83c3caeb299e9b4266db0edf9eaefd23f038da5
Austin Miller <austin.miller@colorado.edu>
2020-02-29 19:25:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,21 @@ def map_to_no_change(source_array)
 end 
 
 def map_to_double(source_array)
+      index = 0 
+  new_array = []
+  while index < source_array.length do 
+    new_array << source_array[index] * 2 
+    index += 1 
+  end 
+  new_array
 end 
 
 def map_to_square(source_array)
+        index = 0 
+  new_array = []
+  while index < source_array.length do 
+    new_array << source_array[index]  
+    index += 1 
+  end 
+  new_array
 end 
