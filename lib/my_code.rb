04467e1454920cc608572a214a40f393c186b5c4
Austin Miller <austin.miller@colorado.edu>
2020-02-29 19:23:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,19 @@
 # My Code here....
-map_to_negativize(source_array)
-map_to_negativize(source_array)
-map_to_negativize(source_array)
-map_to_negativize(source_array)
+def map_to_negativize(source_array)
+  index = 0 
+  new_array = []
+  while index < source_array.length do 
+    new_array << source_array[index] * 1 
+    index += 1 
+  end 
+  new_array
+end 
+
+def map_to_no_change(source_array)
+end 
+
+def map_to_double(source_array)
+end 
+
+def map_to_square(source_array)
+end 
